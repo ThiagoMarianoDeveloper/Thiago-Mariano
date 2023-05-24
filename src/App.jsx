a815c55769d0ecc426ccefef8f27067e1a6f284b
Thiago Mariano <134088653+ThiagoMarianoDeveloper@users.noreply.github.com>
2023-05-24 19:13:54 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Thiago Mariano",
+  title: "Marketing Digital e Desenvolvimento",
+  email: "thiagomarianotvdigital321@gmail.com",
+  gitHub: "thiagomarianodeveloper",
+  instagram: "thiagomariano",
 };
 
 const primaryColor = "#4E567E";
